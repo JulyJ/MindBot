bd0fdc8fed17b8c37dd4253f4f596f26329dc798
Julia <julia.koveshnikova@gmail.com>
2017-04-26 21:20:32 +0300

    - logging level
    DEBUG -> ERROR
    + requirements

--- mindbot/config.py
+++ mindbot/config.py
@@ -1,4 +1,4 @@
-telegram_token = '288232604:AAGWRCk1wdpBNZVdC46dbtWXOOjKLTpo6Gk'
+telegram_token = '365971505:AAESxSrGVlWYsbEgr5F4f_t7VqvmYo2Z9vA'
 db_connection_string = 'sqlite:///minddump.db'
 telegram_url = 'https://api.telegram.org/bot{}/'
 poll_interval = 5
