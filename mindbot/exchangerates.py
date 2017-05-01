0a4838139242937287ce03bb5d7c39c1cf8454db
Julia <julia.koveshnikova@gmail.com>
2017-05-01 13:56:09 +0300

    Currency Exchange

---
+++ mindbot/exchangerates.py
@@ -0,0 +1,27 @@
+from logging import getLogger
+from urllib.parse import urlencode
+
+from requests import Response, get as http_get
+
+from .config import exchangesrates_url
+
+
+class OpenExchangeRatesClient:
+    def __init__(self):
+        self._logger = getLogger(__name__)
+
+    def get_json(self, basecurrency):
+        url = '{base}{question}{query}'.format(
+            base=exchangesrates_url,
+            question='?',
+            query=urlencode({'base': basecurrency})
+        )
+        self._logger.debug('OpenExchangeRates API requested {url}'.format(url=url))
+        return http_get(url).json()
+
+    def get_rate(self, basecurrency, targetcurrency):
+        rates = self.get_json(basecurrency)['rates']
+        if targetcurrency in rates:
+            return rates[targetcurrency]
+        else:
+            return None
