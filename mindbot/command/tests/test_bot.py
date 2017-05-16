7649515b5852957c5b71a7c89ad6529a897ae08f
Julia <julia.koveshnikova@gmail.com>
2017-05-17 00:47:12 +0300

    +test

--- mindbot/command/tests/test_bot.py
+++ mindbot/command/tests/test_bot.py
@@ -3,10 +3,12 @@ import json
 import requests_mock
 
 from mindbot.bot import MindBot
+from mindbot.router import CommandRouter
 from mindbot.telegram import TelegramClient
 from mindbot.config import TELEGRAM_TOKEN
 
 bot = MindBot()
+router = CommandRouter()
 client = TelegramClient(TELEGRAM_TOKEN)
 
 
@@ -60,6 +62,28 @@ def fake_result_message():
         }
 
 
+def get_commands():
+    commands_list = []
+    for command in CommandRouter.get_commands_help():
+        commands_list.append(command[0])
+    return commands_list
+
+
+@pytest.fixture()
+def fake_commands_message(request):
+    return [{
+        "update_id": 547461412,
+        "message": {
+            "message_id": 1486,
+            "from": fake_user(),
+            "chat": test_chat(),
+            "date": 1494875753,
+            "text": request.param,
+            "entities": fake_entities()
+            }
+        }]
+
+
 @pytest.fixture()
 def fake_updates():
     return {
@@ -68,8 +92,45 @@ def fake_updates():
     }
 
 
+@pytest.fixture(params=get_commands())
+def fake_command_updates(request):
+    return {
+        "ok": "true",
+        "result": fake_commands_message(request)
+    }
+
+
+@pytest.fixture()
+def fake_empty_updates():
+    return {
+        "ok": "true",
+        "result": []
+    }
+
+
 def test_get_updates():
     with requests_mock.mock() as m:
         m.get('https://api.telegram.org/bot{}/getUpdates'.format(TELEGRAM_TOKEN),
               text=json.dumps(fake_updates()))
         assert bot.get_updates() == [fake_result_message()]
+
+
+def test_get_empty_updates():
+    with requests_mock.mock() as m:
+        m.get('https://api.telegram.org/bot{}/getUpdates'.format(TELEGRAM_TOKEN),
+              text=json.dumps(fake_empty_updates()))
+        assert bot.get_updates() == []
+
+
+def test_set_last_update_id():
+    with requests_mock.mock() as m:
+        m.get('https://api.telegram.org/bot{}/getUpdates'.format(TELEGRAM_TOKEN),
+              text=json.dumps(fake_updates()))
+        bot.set_last_update_id([fake_result_message()])
+        assert bot._last_update_id == 547461412 + 1
+
+
+def test_route_message(fake_command_updates):  # TODO
+    with requests_mock.mock() as m:
+        m.get(requests_mock.ANY, text='test')
+        bot.route_message([fake_command_updates])
